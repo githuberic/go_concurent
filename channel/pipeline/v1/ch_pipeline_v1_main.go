036ad8c5dae3da847cbdaadfd5e31f0e378f7873
lgq <liugq@weipaitang.com>
2021-06-05 22:15:02 +0800

    add channel pipeline practices

---
+++ channel/pipeline/v1/ch_pipeline_v1_main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+/**
+all goroutines are asleep - deadlock!
+*/
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	go func() {
+		defer close(naturals)
+		for x := 0; ; x++ {
+			naturals <- x
+		}
+	}()
+
+	go func() {
+		defer close(squares)
+		for {
+			x := <-naturals
+			squares <- x * x
+		}
+	}()
+
+	// <-squares 处，期望从管道中拿到数据，而这个数据是其他goroutine协程放入管道
+	// 但是其他goroutine线都已经执行完了(all goroutines are asleep)
+	// 那么就永远不会有数据放入管道。所以，main goroutine线在等一个永远不会来的数据，
+	// 那整个程序就永远等下去了，所以程序down掉。
+	for {
+		fmt.Println(<-squares)
+	}
+}
